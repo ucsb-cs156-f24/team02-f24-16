2507600e342a547ef14deab57d22f751b073de0d
Kelly Li <kellyli@ucsb.edu>
2024-11-03 10:55:18 -0800

    hopefully fixed the formatting

--- frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -1,35 +1,35 @@
 const ucsbdiningcommonsmenuitemFixtures = {
-    oneUcsbDiningCommonsMenuItem: [
-      {
-        id: 1,
-        diningcommonscode: "ortega",
-        name: "Baked Pesto Pasta with Chicken",
-        station: "Entree Specials",
-      },
-    ],
+  oneUcsbDiningCommonsMenuItem: [
+    {
+      id: 1,
+      diningcommonscode: "ortega",
+      name: "Baked Pesto Pasta with Chicken",
+      station: "Entree Specials",
+    },
+  ],
+
+  threeUcsbDiningCommonsMenuItems: [
+    {
+      id: 2,
+      diningcommonscode: "ortega",
+      name: "Tofu Banh Mi Sandwich (v)",
+      station: "Entree Specials",
+    },
+
+    {
+      id: 3,
+      diningcommonscode: "ortega",
+      name: "Chicken Caesar Salad",
+      station: "Entrees",
+    },
+
+    {
+      id: 4,
+      diningcommonscode: "portola",
+      name: "Cream of Broccoli Soup (v)",
+      station: "Greens & Grains",
+    },
+  ],
+};
   
-    threeUcsbDiningCommonsMenuItems: [
-      {
-        id: 2,
-        diningcommonscode: "ortega",
-        name: "Tofu Banh Mi Sandwich (v)",
-        station: "Entree Specials",
-      },
-  
-      {
-        id: 3,
-        diningcommonscode: "ortega",
-        name: "Chicken Caesar Salad",
-        station: "Entrees",
-      },
-  
-      {
-        id: 4,
-        diningcommonscode: "portola",
-        name: "Cream of Broccoli Soup (v)",
-        station: "Greens & Grains",
-      },
-    ],
-  };
-  
-  export { ucsbdiningcommonsmenuitemFixtures };
+export { ucsbdiningcommonsmenuitemFixtures };
