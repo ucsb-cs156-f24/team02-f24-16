2f4bf83a832a773cc8b29dc049f0f7554587df8e
ttnguyen908 <ttnguyen@ucsb.edu>
2024-11-04 23:00:10 -0800

    HelpRequest components for form and table have been added!

---
+++ frontend/src/stories/components/HelpRequest/HelpRequestTable.stories.js
@@ -0,0 +1,41 @@
+import React from "react";
+import HelpRequestTable from "main/components/HelpRequest/HelpRequestTable";
+import { helpRequestFixtures } from "fixtures/HelpRequestFixtures";
+import { currentUserFixtures } from "fixtures/currentUserFixtures";
+import { http, HttpResponse } from "msw";
+
+export default {
+  title: "components/HelpRequest/HelpRequestTable",
+  component: HelpRequestTable,
+};
+
+const Template = (args) => {
+  return <HelpRequestTable {...args} />;
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+  dates: [],
+};
+
+export const ThreeItemsOrdinaryUser = Template.bind({});
+
+ThreeItemsOrdinaryUser.args = {
+  dates: helpRequestFixtures.threeRequests,
+  currentUser: currentUserFixtures.userOnly,
+};
+
+export const ThreeItemsAdminUser = Template.bind({});
+ThreeItemsAdminUser.args = {
+  dates: helpRequestFixtures.threeRequests,
+  currentUser: currentUserFixtures.adminUser,
+};
+
+ThreeItemsAdminUser.parameters = {
+  msw: [
+    http.delete("/api/helprequest", () => {
+      return HttpResponse.json({}, { status: 200 });
+    }),
+  ],
+};
