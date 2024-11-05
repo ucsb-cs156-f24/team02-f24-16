f87358c721353c7eb6506fcb5f5f3ecfbf1a2b30
ttnguyen908 <ttnguyen@ucsb.edu>
2024-11-05 00:01:48 -0800

    HelpRequest fixtures and table component have been edited!

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -6,7 +6,7 @@ const helpRequestFixtures = {
     tableOrBreakoutRoom: "Table_16",
     requestTime: "2024-11-04T12:45:00",
     explanation: "Needs_help_with_jpa03",
-    solved: false,
+    solved: true,
   },
   threeRequests: [
     {
@@ -25,7 +25,7 @@ const helpRequestFixtures = {
       tableOrBreakoutRoom: "Table_01",
       requestTime: "2003-04-20T06:09:00",
       explanation: "Needs_help_with_team01",
-      solved: false,
+      solved: true,
     },
     {
       id: 3,
@@ -34,7 +34,7 @@ const helpRequestFixtures = {
       tableOrBreakoutRoom: "Table_02",
       requestTime: "2000-11-15T00:00:00",
       explanation: "Needs_help_with_jpa00",
-      solved: false,
+      solved: true,
     },
   ],
 };
