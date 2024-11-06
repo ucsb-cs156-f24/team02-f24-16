c69846463eaf5ca018a0c894948204de95c1ba66
cs11111111 <song991@ucsb.edu>
2024-11-06 01:35:24 -0800

    placeholder org

--- frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.js
+++ frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.js
@@ -1,42 +1,17 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import UCSBOrganizationCreatePage from "main/pages/UCSBOrganization/UCSBOrganizationCreatePage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-
-const mockToast = jest.fn();
-jest.mock("react-toastify", () => {
-  const originalModule = jest.requireActual("react-toastify");
-  return {
-    __esModule: true,
-    ...originalModule,
-    toast: (x) => mockToast(x),
-  };
-});
-
-const mockNavigate = jest.fn();
-jest.mock("react-router-dom", () => {
-  const originalModule = jest.requireActual("react-router-dom");
-  return {
-    __esModule: true,
-    ...originalModule,
-    Navigate: (x) => {
-      mockNavigate(x);
-      return null;
-    },
-  };
-});
 
 describe("UCSBOrganizationCreatePage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
 
-  beforeEach(() => {
-    jest.clearAllMocks();
+  const setupUserOnly = () => {
     axiosMock.reset();
     axiosMock.resetHistory();
     axiosMock
@@ -45,10 +20,15 @@ describe("UCSBOrganizationCreatePage tests", () => {
     axiosMock
       .onGet("/api/systemInfo")
       .reply(200, systemInfoFixtures.showingNeither);
-  });
+  };
 
   const queryClient = new QueryClient();
-  test("renders without crashing", async () => {
+  test("Renders expected content", async () => {
+    // arrange
+
+    setupUserOnly();
+
+    // act
     render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
@@ -57,59 +37,8 @@ describe("UCSBOrganizationCreatePage tests", () => {
       </QueryClientProvider>,
     );
 
-    await waitFor(() => {
-      expect(screen.getByLabelText("Name")).toBeInTheDocument();
-    });
-  });
+    // assert
 
-  test("on submit, makes request to backend, and redirects to /organizations", async () => {
-    const queryClient = new QueryClient();
-    const organization = {
-      orgCode: "SKY",
-      name: "South Coast Deli",
-      description: "Sandwiches and Salads",
-    };
-
-    axiosMock.onPost("/api/organizations/post").reply(202, organization);
-
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <UCSBOrganizationCreatePage />
-        </MemoryRouter>
-      </QueryClientProvider>,
-    );
-
-    await waitFor(() => {
-      expect(screen.getByLabelText("Name")).toBeInTheDocument();
-    });
-
-    const nameInput = screen.getByLabelText("Name");
-    expect(nameInput).toBeInTheDocument();
-
-    const descriptionInput = screen.getByLabelText("Description");
-    expect(descriptionInput).toBeInTheDocument();
-
-    const createButton = screen.getByText("Create");
-    expect(createButton).toBeInTheDocument();
-
-    fireEvent.change(nameInput, { target: { value: "South Coast Deli" } });
-    fireEvent.change(descriptionInput, {
-      target: { value: "Sandwiches and Salads" },
-    });
-    fireEvent.click(createButton);
-
-    await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
-
-    expect(axiosMock.history.post[0].params).toEqual({
-      name: "South Coast Deli",
-      description: "Sandwiches and Salads",
-    });
-
-    // assert - check that the toast was called with the expected message
-    expect(mockToast).toBeCalledWith(
-      "New organization Created - orgCode: 3 name: South Coast Deli",
-    );
-    expect(mockNavigate).toBeCalledWith({ to: "/organizations" });
+    await screen.findByText("Create page not yet implemented");
   });
 });
