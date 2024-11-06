470b4f9a37f3e13ceeb321676f51132b131ccbdd
rohanpreetam <rohanpreetam@ucsb.edu>
2024-11-05 18:12:10 -0800

    Created Fixtures for MenuItemReview table

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -1,34 +1,34 @@
 const menuItemReviewFixtures = {
-    oneReview: {
+  oneReview: {
+    id: 1,
+    reviewerEmail: "rohanpreetam21@gmail.com",
+    stars: 5,
+    dateReviewed: "2022-01-02T12:00:00",
+    comments: "Very good",
+  },
+  threeReviews: [
+    {
       id: 1,
       reviewerEmail: "rohanpreetam21@gmail.com",
       stars: 5,
       dateReviewed: "2022-01-02T12:00:00",
       comments: "Very good",
     },
-    threeReviews: [
-      {
-        id: 1,
-        reviewerEmail: "rohanpreetam21@gmail.com",
-        stars: 5,
-        dateReviewed: "2022-01-02T12:00:00",
-        comments: "Very good",
-      },
-      {
-        id: 2,
-        reviewerEmail: "rohanpreetam@ucsb.com",
-        stars: 4,
-        dateReviewed: "2022-04-03T12:00:00",
-        comments: "Good",
-      },
-      {
-        id: 3,
-        reviewerEmail: "salshriaan@ucsb.com",
-        stars: 3,
-        dateReviewed: "2022-07-04T12:00:00",
-        comments: "Very average",
-      },
-    ],
-  };
-  
-  export { menuItemReviewFixtures };
+    {
+      id: 2,
+      reviewerEmail: "rohanpreetam@ucsb.com",
+      stars: 4,
+      dateReviewed: "2022-04-03T12:00:00",
+      comments: "Good",
+    },
+    {
+      id: 3,
+      reviewerEmail: "salshriaan@ucsb.com",
+      stars: 3,
+      dateReviewed: "2022-07-04T12:00:00",
+      comments: "Very average",
+    },
+  ],
+};
+
+export { menuItemReviewFixtures };
