8e60ba75dc6c25929056ccb24cfcfe6027ffd18a
Jayden <82332696+jayster7101@users.noreply.github.com>
2024-11-05 19:31:05 -0800

    Recommendation Request page setup, blank pages

--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.js
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.js
@@ -1,49 +1,17 @@
-import React from "react";
-import { useBackend } from "main/utils/useBackend";
-
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import RecommendationRequestTable from "main/components/RecommendationRequest/RecommendationRequestTable";
-import { useCurrentUser, hasRole } from "main/utils/currentUser";
-import { Button } from "react-bootstrap";
 
 export default function RecommendationRequestIndexPage() {
-  const currentUser = useCurrentUser();
-
-  const {
-    data: request,
-    error: _error,
-    status: _status,
-  } = useBackend(
-    // Stryker disable next-line all : don't test internal caching of React Query
-    ["/api/recommendationRequest/all"],
-    { method: "GET", url: "/api/recommendationRequest/all" },
-    // Stryker disable next-line all : don't test default value of empty list
-    [],
-  );
-
-  const createButton = () => {
-    if (hasRole(currentUser, "ROLE_ADMIN")) {
-      return (
-        <Button
-          variant="primary"
-          href="/recommendationRequest/create"
-          style={{ float: "right" }}
-        >
-          Create recommendationRequest
-        </Button>
-      );
-    }
-  };
-
+  // Stryker disable all : placeholder for future implementation
   return (
     <BasicLayout>
       <div className="pt-2">
-        {createButton()}
-        <h1>recommendationRequest</h1>
-        <RecommendationRequestTable
-          request={request}
-          currentUser={currentUser}
-        />
+        <h1>Index page not yet implemented</h1>
+        <p>
+          <a href="/placeholder/create">Create</a>
+        </p>
+        <p>
+          <a href="/placeholder/edit/1">Edit</a>
+        </p>
       </div>
     </BasicLayout>
   );
--- frontend/src/tests/pages/RecommendationRequest/RecommendationRequestCreatePage.test.js
+++ frontend/src/tests/pages/RecommendationRequest/RecommendationRequestCreatePage.test.js
@@ -1,4 +1,4 @@
-import { render, waitFor, fireEvent, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import RecommendationRequestCreatePage from "main/pages/RecommendationRequest/RecommendationRequestCreatePage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
@@ -8,33 +8,10 @@ import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
-const mockToast = jest.fn();
-jest.mock("react-toastify", () => {
-  const originalModule = jest.requireActual("react-toastify");
-  return {
-    __esModule: true,
-    ...originalModule,
-    toast: (x) => mockToast(x),
-  };
-});
-
-const mockNavigate = jest.fn();
-jest.mock("react-router-dom", () => {
-  const originalModule = jest.requireActual("react-router-dom");
-  return {
-    __esModule: true,
-    ...originalModule,
-    Navigate: (x) => {
-      mockNavigate(x);
-      return null;
-    },
-  };
-});
-
 describe("RecommendationRequestCreatePage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
 
-  beforeEach(() => {
+  const setupUserOnly = () => {
     axiosMock.reset();
     axiosMock.resetHistory();
     axiosMock
@@ -43,10 +20,15 @@ describe("RecommendationRequestCreatePage tests", () => {
     axiosMock
       .onGet("/api/systemInfo")
       .reply(200, systemInfoFixtures.showingNeither);
-  });
+  };
 
-  test("renders without crashing", async () => {
-    const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
+  test("Renders expected content", async () => {
+    // arrange
+
+    setupUserOnly();
+
+    // act
     render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
@@ -55,79 +37,8 @@ describe("RecommendationRequestCreatePage tests", () => {
       </QueryClientProvider>,
     );
 
-    await waitFor(() => {
-      expect(
-        screen.getByTestId("RecommendationRequestForm-professorEmail"),
-      ).toBeInTheDocument();
-    });
-  });
+    // assert
 
-  test("when you fill in the form and hit submit, it makes a request to the backend", async () => {
-    const queryClient = new QueryClient();
-    const request = {
-        id: 3,
-        requesterEmail: "jaydenjardine@cs.ucsb.edu",
-        professorEmail: "rihanna.fenty@fentymusic.com",
-        explanation: "Requesting your participation in a university event focusing on the influence of Caribbean music in pop culture.",
-        dateRequested: "2024-10-31T15:30:00",
-        dateNeeded: "2024-11-15T15:30:00",
-        done: false,
-    };
-
-    axiosMock.onPost("/api/recommendationRequest/post").reply(202, request);
-
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <RecommendationRequestCreatePage />
-        </MemoryRouter>
-      </QueryClientProvider>,
-    );
-
-    await waitFor(() => {
-      expect(
-        screen.getByTestId("RecommendationRequestForm-professorEmail"),
-      ).toBeInTheDocument();
-    });
-
-    const testId = "RecommendationRequestTable";
-    const testIdButton = "recommendationRequestTable";
-
-    const requesterEmailField = screen.getByTestId(`${testId}-requesterEmail`);
-    const professorEmailField = screen.getByTestId(`${testId}-professorEmail`);
-    const explanationField = screen.getByTestId(`${testId}-explanation`);
-    const dateRequestedField = screen.getByTestId(`${testId}-dateRequested`);
-    const dateNeededField = screen.getByTestId(`${testId}-dateNeeded`);
-    const doneField = screen.getByTestId(`${testId}-done`);
-
-
-    const submitButton = screen.getByTestId(`${testIdButton}-submit`);
-
-    fireEvent.change(requesterEmailField, { target: { value: "jaydenjardine@ucsb.edu" } });
-    fireEvent.change(professorEmailField, { target: { value: "rihanna.fenty@fentymusic.com" } });
-    fireEvent.change(explanationField, { target: { value: "Requesting your participation in a university event focusing on the influence of Caribbean music in pop culture." } });
-    fireEvent.change(dateRequestedField, { target: { value: "2024-10-31T15:30:00" } });
-    fireEvent.change(dateNeededField, { target: { value: "2024-11-15T15:30:00" } });
-    fireEvent.change(doneField, { target: { value: "true" } });
-
-    expect(submitButton).toBeInTheDocument();
-
-    fireEvent.click(submitButton);
-
-    await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
-
-    expect(axiosMock.history.post[0].params).toEqual({
-        id: 3,
-        requesterEmail: "jaydenjardine@cs.ucsb.edu",
-        professorEmail: "rihanna.fenty@fentymusic.com",
-        explanation: "Requesting your participation in a university event focusing on the influence of Caribbean music in pop culture.",
-        dateRequested: "2024-10-31T15:30:00",
-        dateNeeded: "2024-11-15T15:30:00",
-        done: false,
-    });
-
-    expect(mockToast).toBeCalledWith(
-      "New RecommendationRequest Created -  id: 3, requesterEmail: jaydenjardine@cs.ucsb.edu, professorEmail: rihanna.fenty@fentymusic.com, explanation: Requesting your participation in a university event focusing on the influence of Caribbean music in pop culture., dateRequested: 2024-10-31T15:30:00, dateNeeded: 2024-11-15T15:30:00, done: false");
-    expect(mockNavigate).toBeCalledWith({ to: "/recommendationRequest" });
+    await screen.findByText("Create page not yet implemented");
   });
 });
