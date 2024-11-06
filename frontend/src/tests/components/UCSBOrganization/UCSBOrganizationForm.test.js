f7eba8cc9a0dd7269c741dc2b46b502363ffba8e
cs11111111 <song991@ucsb.edu>
2024-11-05 16:26:32 -0800

    form component org

---
+++ frontend/src/tests/components/UCSBOrganization/UCSBOrganizationForm.test.js
@@ -0,0 +1,123 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import UCSBOrganizationForm from "main/components/UCSBOrganization/UCSBOrganizationForm";
+import { ucsbOrganizationFixtures } from "fixtures/ucsbOrganizationFixtures";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const mockedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedNavigate,
+}));
+
+describe("UCSBOrganizationForm tests", () => {
+  const queryClient = new QueryClient();
+
+  const expectedHeaders = ["orgCode", "orgTranslationShort", "orgTranslation", "inactive"];
+  const testId = "UCSBOrganizationForm";
+
+  test("renders correctly with no initialContents", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+  });
+
+  test("renders correctly when passing in initialContents", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm initialContents={ucsbOrganizationFixtures.oneOrganization} />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+
+    expect(await screen.findByTestId(`${testId}-id`)).toBeInTheDocument();
+    expect(screen.getByText(`Id`)).toBeInTheDocument();
+  });
+
+  test("that navigate(-1) is called when Cancel is clicked", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+    expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
+    const cancelButton = screen.getByTestId(`${testId}-cancel`);
+
+    fireEvent.click(cancelButton);
+
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+  });
+
+  test("that the correct validations are performed", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+    const submitButton = screen.getByText(/Create/);
+    fireEvent.click(submitButton);
+
+    await screen.findByText(/orgCode is required/);
+    expect(screen.getByText(/orgTranslationShort is required/)).toBeInTheDocument();
+    expect(screen.getByText(/orgTranslation is required/)).toBeInTheDocument();
+    expect(screen.getByText(/Inactive status is required/)).toBeInTheDocument();
+
+    const nameInput = screen.getByTestId(`${testId}-orgCode`);
+    fireEvent.change(nameInput, { target: { value: "a".repeat(31) } });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(screen.getByText(/Max length 30 characters/)).toBeInTheDocument();
+    });
+  });
+
+  // test("renders correctly with initialContents and handles submit", async () => {
+  //   const submitAction = jest.fn();
+  //   render(
+  //     <QueryClientProvider client={queryClient}>
+  //       <Router>
+  //         <UCSBOrganizationForm initialContents={organizationFixtures.oneOrganization[0]} submitAction={submitAction} />
+  //       </Router>
+  //     </QueryClientProvider>,
+  //   );
+
+  //   const orgCodeInput = screen.getByTestId(`${testId}-orgCode`);
+  //   fireEvent.change(orgCodeInput, { target: { value: "ZETA" } });
+
+  //   const submitButton = screen.getByTestId(`${testId}-submit`);
+  //   fireEvent.click(submitButton);
+
+  //   await waitFor(() => {
+  //     expect(submitAction).toHaveBeenCalled();
+  //   });
+  // });
+
+});
