54340df24823fc339268219e0b8453c5683f8bd0
rohanpreetam <rohanpreetam@ucsb.edu>
2024-11-07 15:07:22 -0800

    Prettier ran for different files

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -1,5 +1,14 @@
 const menuItemReviewFixtures = {
-    oneMenuItemReview: {
+  oneMenuItemReview: {
+    id: 1,
+    itemId: 10,
+    reviewerEmail: "rohanpreetam21@gmail.com",
+    stars: 5,
+    dateReviewed: "2022-01-02T12:00:00",
+    comments: "Very good",
+  },
+  threeMenuItemReviews: [
+    {
       id: 1,
       itemId: 10,
       reviewerEmail: "rohanpreetam21@gmail.com",
@@ -7,32 +16,23 @@ const menuItemReviewFixtures = {
       dateReviewed: "2022-01-02T12:00:00",
       comments: "Very good",
     },
-    threeMenuItemReviews: [
-      {
-        id: 1,
-        itemId: 10,
-        reviewerEmail: "rohanpreetam21@gmail.com",
-        stars: 5,
-        dateReviewed: "2022-01-02T12:00:00",
-        comments: "Very good",
-      },
-      {
-        id: 2,
-        itemId: 20,
-        reviewerEmail: "rohanpreetam@ucsb.com",
-        stars: 4,
-        dateReviewed: "2022-04-03T12:00:00",
-        comments: "Good",
-      },
-      {
-        id: 3,
-        itemId: 30,
-        reviewerEmail: "salshriaan@ucsb.com",
-        stars: 3,
-        dateReviewed: "2022-07-04T12:00:00",
-        comments: "Very average",
-      },
-    ],
-  };
-  
-  export { menuItemReviewFixtures };
+    {
+      id: 2,
+      itemId: 20,
+      reviewerEmail: "rohanpreetam@ucsb.com",
+      stars: 4,
+      dateReviewed: "2022-04-03T12:00:00",
+      comments: "Good",
+    },
+    {
+      id: 3,
+      itemId: 30,
+      reviewerEmail: "salshriaan@ucsb.com",
+      stars: 3,
+      dateReviewed: "2022-07-04T12:00:00",
+      comments: "Very average",
+    },
+  ],
+};
+
+export { menuItemReviewFixtures };
