32e54fdc9692232a49abe98dae512a3457b493e0
Kelly Li <kellyli@ucsb.edu>
2024-11-06 18:15:17 -0800

    changed link

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -69,7 +69,7 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/ucsbdates">
                     UCSB Dates
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/diningcommonsmenuitem">
+                  <Nav.Link as={Link} to="/ucsbdiningcommonsmenuitem">
                     UCSB Dining Commons Menu Item
                   </Nav.Link>
                   <Nav.Link as={Link} to="/placeholder">
